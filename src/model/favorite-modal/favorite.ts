ceb616b7a1d23aed1d443b8ba75364e1ea0f6f2f
rithyp <rithyp@Huots-MacBook-Pro.local>
2022-02-16 09:19:56 +0700

    finalize

---
+++ src/model/favorite-modal/favorite.ts
@@ -0,0 +1,17 @@
+export class Favorite {
+
+    id:string;
+    name:string;
+    vc:string
+    projectName:string;
+    favorite:boolean
+
+
+    constructor() {
+        this.id = "";
+        this.name = "";
+        this.vc = "";
+        this.projectName = "";
+        this.favorite = false;
+    }
+}
---
+++ src/model/student-modal/student.ts
@@ -0,0 +1,18 @@
+export class Student {
+
+    id:string;
+    name:string;
+    vc:string
+    projectName:string;
+    favorite: boolean;
+
+
+    constructor() {
+        this.id = "";
+        this.name = "";
+        this.vc = "";
+        this.projectName = "";
+        this.favorite = false;
+    }
+
+}
